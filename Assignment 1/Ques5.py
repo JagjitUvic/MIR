5732dbe763f8a63b1fa561ec728b467f42908d35
JagjitUvic <jagjitsingh.mail@gmail.com>
2017-01-30 03:59:31 -0800

    Question 5 3

--- Assignment 1/Ques5.py
+++ Assignment 1/Ques5.py
@@ -10,6 +10,8 @@ sig = Signal()
 #Read audio File
 sig.wav_read('Ques5Input.wav')
 wave = sig.data
-wavefft = np.fft.ifft(wave)
+wavefft = np.fft.fft(wave)
+waveifft = np.fft.ifft(wavefft)
 #Writing the audio file
+sig = Signal(data=waveifft)
 sig.wav_write('Ques5Output.wav')
